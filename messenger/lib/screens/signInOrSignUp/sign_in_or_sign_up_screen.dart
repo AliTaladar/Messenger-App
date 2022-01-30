2a21d1e9d533f382e229d41084e3a21c1278c052
Ali Taladar <taladarali@gmail.com>
2022-01-29 19:42:02 -0500

    Made welcome and signin/signup

---
+++ messenger/lib/screens/signInOrSignUp/sign_in_or_sign_up_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:messenger/components/primary_button.dart';
+import 'package:messenger/constants.dart';
+
+class SigninOrSignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            children: [
+              Spacer(
+                flex: 2,
+              ),
+              Image.asset(
+                MediaQuery.of(context).platformBrightness == Brightness.light
+                    ? 'assets/images/Logo_light.png'
+                    : 'assets/images/Logo_dark.png',
+                height: 146,
+              ),
+              Spacer(),
+              PrimaryButton(
+                text: 'Sign In',
+                press: () {},
+              ),
+              SizedBox(
+                height: kDefaultPadding * 1.5,
+              ),
+              PrimaryButton(
+                color: Theme.of(context).colorScheme.secondary,
+                text: 'Sign Un',
+                press: () {},
+              ),
+              Spacer(
+                flex: 2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
